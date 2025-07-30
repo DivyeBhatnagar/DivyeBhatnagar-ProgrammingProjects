080139b5e026d0798ab995d61339cd2d111845ba
Divye Bhatnagar <divyebhatnagar784@gmail.com>
2025-07-30 16:58:50 +0530

    Add Java implementation and update VS Code configuration

---
+++ Java/main.java
@@ -0,0 +1,8 @@
+package Java;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.print("Divye Bhatnagar");
+        System.out.print("\nAce Platinum");
+    }
+}

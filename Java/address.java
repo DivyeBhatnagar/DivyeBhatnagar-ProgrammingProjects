8a2bf2bbb15cd1f118794d59b6b264efb8b969f4
Divye Bhatnagar <divyebhatnagar784@gmail.com>
2025-08-06 16:29:34 +0530

    Add Java files and update project structure

---
+++ Java/address.java
@@ -0,0 +1,7 @@
+public class address {
+    public static void main(String[] args) {
+        System.out.print("Divye Bhatnagar\n");
+        System.out.println("Ace Platinum");
+        System.out.println("Greater Noida");
+    }
+}
--- Java/main.java
+++
@@ -1,8 +0,0 @@
-package Java;
-
-public class main {
-    public static void main(String[] args) {
-        System.out.print("Divye Bhatnagar");
-        System.out.print("\nAce Platinum");
-    }
-}
